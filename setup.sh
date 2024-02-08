b931c9542c2c4c17d2e91c4ce0803c18e077a60e
Shane Brown <shane.brown@Shanes-16-inch-MBP.local>
2024-02-08 08:25:32 -0600

    adding setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# will clean this up later; testing generic workflow for now
+
+# get logged-in user
+loggedInUser=$( scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }' )
+
+ln -s /Users/$loggedInUser/dotfiles/system/.zshrc /Users/$loggedInUser/.zshrc
+ln -s /Users/$loggedInUser/dotfiles/system/.vimrc /Users/$loggedInUser/.vimrc
+ln -s /Users/$loggedInUser/dotfiles/system/.aliases /Users/$loggedInUser/.aliases
