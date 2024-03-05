e280e7849273df5d171e75bb0dc60997e266d77c
shanembrown <21061399+shanembrown@users.noreply.github.com>
2024-03-05 14:25:21 -0600

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,11 @@
 # get logged-in user
 loggedInUser=$( scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }' )
 
-ln -s /Users/$loggedInUser/dotfiles/system/.zshrc /Users/$loggedInUser/.zshrc
-ln -s /Users/$loggedInUser/dotfiles/system/.vimrc /Users/$loggedInUser/.vimrc
-ln -s /Users/$loggedInUser/dotfiles/system/.aliases /Users/$loggedInUser/.aliases
-ln -s /Users/$loggedInUser/dotfiles/system/.functions /Users/$loggedInUser/.functions
+# use find to get the dotfile filenames
+# loop through to create symlinks
+for dotFile in `find /Users/$loggedInUser/dotfiles/system -type f -exec basename {} \;`; do; `ln -s /Users/$loggedInUser/dotfiles/system/$dotFile /Users/$loggedInUser/$dotFile`; done
+
+#ln -s /Users/$loggedInUser/dotfiles/system/.zshrc /Users/$loggedInUser/.zshrc
+#ln -s /Users/$loggedInUser/dotfiles/system/.vimrc /Users/$loggedInUser/.vimrc
+#ln -s /Users/$loggedInUser/dotfiles/system/.aliases /Users/$loggedInUser/.aliases
+#ln -s /Users/$loggedInUser/dotfiles/system/.functions /Users/$loggedInUser/.functions
